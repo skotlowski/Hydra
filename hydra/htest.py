af6927189ea579657e9ae1df139c5d0e54849ff7
Szymon Kotlowski <szymonkotlowski20@gmail.com>
2020-10-14 22:39:57 +0200

    assertTrue function implementation and Testing class

--- hydra/htest.py
+++ hydra/htest.py
@@ -2,19 +2,23 @@ from .logger import create_logger
 
 
 class HTest:
-    def __init__(self):
-        self.logger = create_logger(__name__)
+    def __init__(self, class_name):
+        self.logger = create_logger(class_name.__name__)
         
         self.logger.info("Test case initialized")
 
     def assertTrue(self, cond: bool):
         if not cond:
             print("FAILED")
+        else:
+            print("PASSED")
 
     def assertFalse(self, cond: bool):
         pass
 
     def assertEqual(self, value, expected):
+        if value == expected:
+            pass
         pass
 
     def assertNotEqual(self, value, expected):
--- run.py
+++ run.py
@@ -2,3 +2,14 @@ from hydra import HTest
 
 
 # TODO: Create example unit test case
+
+class Testing(HTest):
+    def __init__(self):
+        super().__init__(Testing)
+
+
+test = Testing()
+test.assertTrue(cond=False)
+
+
+
